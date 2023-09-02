ecf25ae19d2899d8e172934543f8d47311a3642b
abhi267266 <abhi267266@gmail.com>
2023-09-02 15:11:21 +0530

    fixed some bugs and encrpted the password before saving to the database

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJs = require('crypto-js');
 
 //Register
 
@@ -7,7 +8,7 @@ router.post('/register', async (req, res)=>{
         const newUser = new User({
             username: req.body.username,
             email:req.body.email,
-            password: req.body.password
+            password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
         });
 
         try{
