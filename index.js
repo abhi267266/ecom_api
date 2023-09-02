0daea9c27cfb509c5630bf080832fa6c6aacf0e7
abhi267266 <abhi267266@gmail.com>
2023-09-02 15:01:37 +0530

    first query to database to add user and it is working

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("db connected success"))
     .catch((e) => console.log(e));
+
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+
 
 
 
