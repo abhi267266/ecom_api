faec6b15339ef0819a1ddf47f798cf3f4d9a6c9c
abhi267266 <abhi267266@gmail.com>
2023-09-03 16:52:28 +0530

    all the routes are complete implementing stripe payment gateway

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 
@@ -19,7 +21,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
-
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 
 
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,6 @@
 
 const router = require('express').Router();
 const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require('./verifyToken');
-const {passEncrypt} = require('../config/cryptojsfun');
 const Product = require('../models/Product');
 
 //Create
@@ -12,94 +11,38 @@ router.post('/', verifyTokenAndAdmin,async (req, res)=>{
         const savedProduct = await newProduct.save();
         res.status(200).json(savedProduct);
     } catch (error) {
+        console.log(error)
         res.status(500).json(err);
     }
+});
+
+
+//get all the products
+
+router.get('/', async (req, res)=>{
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    const qNum = req.query.num;
+     try {
+        let products;
+
+        if(qNew){
+            products = await Product.find().sort({createdAt: -1}).limit(qNum || 5);
+        }else if(qCategory){
+            products = await Product.find({
+                categories:{
+                    $in:[qCategory],
+                }
+            });
+        }else{
+            products = await Product.find();
+        }
+        res.status(200).json(products);
+     } catch (error) {
+          res.status(500).json(error);
+     }
 })
 
-
-// router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
-//    if(req.body.password){
-//         req.body.password = passEncrypt(req.body.password);
-//    }
-
-//    try {
-
-//     const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-//         $set: req.body
-//     }, {new:true});
-
-//     res.status(200).json(updatedUser)
-
-//    } catch (err) {
-//         res.status(304).json(err);
-//    }
-// });
-
-
-// router.delete('/:id', verifyTokenAndAuthorization , async(req, res)=>{
-//      try {
-//           await User.findByIdAndDelete(req.params.id);
-//           res.status(200).json("User Has Been Removed");
-//      } catch (error) {
-//           res.status(500).json(error);
-//      }
-// })
-
-// router.get('/find/:id', verifyTokenAndAdmin , async(req, res)=>{
-//      try {
-//           const user = await User.findById(req.params.id);
-//           const {password, ...others} = user._doc;
-
-//           res.status(200).json(others);
-//      } catch (error) {
-//           res.status(500).json(error);
-//      }
-// })
-
-// //get all the users
-
-// router.get('/', verifyTokenAndAdmin, async (req, res)=>{
-//      try {
-//           const query = req.query.lim;
-
-//           const user = query ? await User.find().sort({_id: -1}).limit(query) : await User.find();
-//           res.status(200).json(user);
-//      } catch (error) {
-//           res.status(500).json(error);
-//      }
-// })
-
-// //Get user stats
-
-// router.get('/stats', verifyTokenAndAdmin, async (req, res)=>{
-//      const date = new Date();
-//      const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-//      console.log(lastYear);
-
-//      try {
-
-//           const data = await User.aggregate([
-//                {$match: {createdAt :{ $gte : lastYear}}},
-//                {$project: {
-//                     month:{$month : "$createdAt"},
-//                }},
-//                {
-//                     $group: {
-//                          _id:'$month',
-//                          total:{$sum: 1}
-//                     }
-//                }
-
-//           ]);
-
-//           res.status(200).json(data);
-          
-//      } catch (err) {
-//           res.status(500).json(err);
-//      }
-// });
-
-
-
-
 module.exports = router;
+
+//1:30:56
