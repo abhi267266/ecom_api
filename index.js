40de7be48c260738f52650e1dd3beb673b6c800c
abhi267266 <abhi267266@gmail.com>
2023-09-03 18:50:28 +0530

    stuck while tring to implements stripe payment

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product')
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe')
+
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,16 +19,17 @@ mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("db connected success"))
     .catch((e) => console.log(e));
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("server is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server is running on port 5000");
 })
